21d9eb397ea8d81933210bc05a68450c90e6144d
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2022-02-23 23:00:22 +0530

    hi commit

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -4,5 +4,6 @@
 #2. Take instance IP and register in DNS.
 
 TEMP_ID="lt-0f74f51dfef2b396f"
+TEMP_VERS=2
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID} | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VERS} | jq
