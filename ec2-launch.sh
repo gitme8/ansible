d46cab0070569898264afe388aaade529e43ad09
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2022-02-23 23:43:53 +0530

    hi commit

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -6,4 +6,4 @@
 TEMP_ID="lt-0dd90876b45053146"
 TEMP_VERS=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VERS} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Frontend}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=Frontend}]"  | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VERS} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=Frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=Frontend}]"  | jq
