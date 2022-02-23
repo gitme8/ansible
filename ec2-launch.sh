95d24159acdc8b641dc3fd4b078c5f5b5ff27789
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2022-02-23 23:26:22 +0530

    hi commit

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,7 +3,7 @@
 #1. create spot instance
 #2. Take instance IP and register in DNS.
 
-TEMP_ID="lt-0f74f51dfef2b396f"
-TEMP_VERS=2
+TEMP_ID="lt-0dd90876b45053146"
+TEMP_VERS=1
 
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VERS} | jq
