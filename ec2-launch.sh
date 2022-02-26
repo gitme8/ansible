27dea219d8f120807a7f2d5922cf742d50cdc9f8
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2022-02-26 21:19:26 +0530

    hi commit

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -11,15 +11,15 @@ COMPONENT=$1
 TEMP_ID="lt-0dd90876b45053146"
 TEMP_VERS=1
 ZONE_ID=Z0147057284KXP13X4MV
-
+CREATE_INSTANCE() {
 ## Check if instance exist already
-aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
-if [ $? -eq -0 ]; then
-  echo -e "\e[1;33mInstance is already there\e[0m"
-else
-  aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VERS} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
-fi
-
+ aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+ if [ $? -eq -0 ]; then
+   echo -e "\e[1;33mInstance is already there\e[0m"
+ else
+   aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VERS} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+ fi
+}
 
 
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=Frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g') | grep -v null
@@ -27,3 +27,12 @@ sed -e "s/IPADDRESS/${IPADDRESS}" -e "s/COMPONENT/${COMPONENT}" record.json >/tm
 
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 ## update DNS record
+
+if  [ "$COMPONENT" == "all" ]; then
+  for comp in frontend mongodb catalogue rabbitmq mysql ; do
+    COMPONENT=$comp
+    CREATE_INSTANCE
+  done
+else
+  CREATE_INSTANCE
+fi
